ac1dd8594c195d7c3e0504b15c5f53c14779b0a7
Dan FC <a329842@uach.mx>
2021-03-30 22:54:25 -0700

    primer commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let list = document.getElementById("lista");
+
+//let valores = ['Irving Rios', 'Luis Fernando', 'Melissa García'];
+let valores = [];
+
+const mostrarValores = () => valores.map(valor => {
+    let componenteLi = document.createElement("li");
+    componenteLi.innerHTML = valor;
+    lista.appendChild(componenteLi);
+})
+
+const agregarValor = () => {
+    let valor = document.getElementById("valor");
+    lista.innerHTML = "";
+    valores.push(valor.value);
+    console.log(valores);
+    mostrarValores();
+    valor.value="";
+}
+
+const limpiarValores = () => {
+    lista.innerHTML = "";
+    valores = [];
+}
+
+mostrarValores();
